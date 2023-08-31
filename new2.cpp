dd77e2cced5d2b4256d16717380339379b973ada
Himanshuu2125 <103322876+Himanshuu2125@users.noreply.github.com>
2023-08-31 17:27:13 +0530

    Add files via upload

---
+++ new2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    int arr[num]={};
+    
+    cin>>num;
+    
+    
+    for(int i=0;i<num;i++)
+
+    {
+        cin>>arr[i];
+        cout<<endl;
+        
+
+    }
+
+    for(int j=0;j<num;j++)
+
+    {
+        cout<<arr[j];
+
+    }
+    
+
+    return 0;
+}
